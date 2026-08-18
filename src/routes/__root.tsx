15902cf8297a6945fa2d7517f224e71ae82cbef8
danielababu <daniel0ababu@gmail.com>
2026-08-18 23:48:49 +0300

    Update Chrome tab favicon to vibrant orange signal target icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
